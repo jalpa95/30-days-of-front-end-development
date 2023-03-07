fc343642323960a6b8d94672d041fb83226be61f
Jalpa Dangariya <jalpa.dangaria@gmail.com>
2023-03-07 14:22:19 +0100

    22nd Day Project added

---
+++ 22-WaveLabel/index.js
@@ -0,0 +1,13 @@
+
+
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach((label) => {
+	label.innerHTML = label.innerText
+		.split('')
+		.map(
+			(letter, index) =>
+				`<span style="transition-delay: ${index * 70}ms">${letter}</span>`
+		)
+		.join('')
+})
